2a1dffb199c13040178a0e5fc7e786ee15986f98
vaseem khan <vasimkhan658@gmail.com>
2023-09-06 21:39:06 +0530

    upadte

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,10 @@
 import config from "config";
 import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
-import HttpException from "exceptions/HttpException";
-import { DataStoredInToken, RequestWithUser } from "interfaces/auth.interface";
+import HttpException from "../exceptions/HttpException";
+import { DataStoredInToken, RequestWithUser } from "../interfaces/auth.interface";
 // import userModel from "@models/users.model";
-import MSG from "utils/locale.en.json";
+import MSG from "../utils/locale.en.json";
 
 const authMiddleware = async (
 	req: RequestWithUser,
