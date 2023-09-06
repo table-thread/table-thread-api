c8ade8d1c26838620329d856c9bb05dcaf5ed454
vaseem khan <vasimkhan658@gmail.com>
2023-09-06 21:16:48 +0530

    update

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -4,7 +4,7 @@ import { dbConfig } from "@interfaces/db.interface";
 const { host, port, database, username, password }: dbConfig = config.get("dbConfig");
 
 export const dbConnection = {
-	url: `mongodb://localhost:27017/test`,
+	url: `mongodb+srv://vaseem:Kn5XmCTQfh36E5DV@cluster0.09qzb0y.mongodb.net/tableThreads-dev`,
 	options: {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
