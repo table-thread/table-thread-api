d47925196e5ec41dc97d085ac0be6d40efab18ff
vaseem khan <vasimkhan658@gmail.com>
2023-09-06 21:55:33 +0530

    upadte

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import HttpException from "exceptions/HttpException";
-import { logger } from "../utils/logger";
 
 const errorMiddleware = (
 	error: HttpException,
@@ -12,7 +11,7 @@ const errorMiddleware = (
 		const status: number = error.status || 500;
 		const message: string = error.message || "Something went wrong";
 
-		logger.error(
+		console.log(
 			`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`
 		);
 		res.status(status).json({ message });
