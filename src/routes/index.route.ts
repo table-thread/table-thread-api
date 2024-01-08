bccadd2b56660f7b06eedd5ed7a20b7658434272
vaseem khan <vasimkhan658@gmail.com>
2024-01-08 17:15:45 +0530

    update

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Route from "@interfaces/routes.interface";
-import authMiddleware from "@middlewares/auth.middleware";
-import IndexController from "@controllers/index.controller";
+// import IndexController from "@controllers/index.controller";
+import IndexController from "../controllers/index.controller";
 class IndexRoute implements Route {
 	public path = "/api";
 	public router = Router();
